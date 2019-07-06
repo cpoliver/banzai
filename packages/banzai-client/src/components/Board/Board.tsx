9f78e6009e119fd19ea56380cc2ac54ee0bfa13d
Charles Oliver <charles@cocoa.consulting>
2019-07-06 15:16:41 +0100

    add Column, Card components

--- packages/banzai-client/src/components/Board/Board.tsx
+++ packages/banzai-client/src/components/Board/Board.tsx
@@ -2,29 +2,31 @@ import React from "react";
 
 import { BoardsQuery } from "../../generated/graphql";
 
-// interface CardProps {
-//   title: String;
-// }
+interface CardProps {
+  title: String;
+}
 
-// const Card: React.FC<CardProps> = props => <div className="c-card">card</div>;
+const Card: React.FC<CardProps> = ({ title }) => (
+  <div className="c-card">{title}</div>
+);
 
-// interface ColumnProps {
-//   title: String;
-//   cards: Card[];
-// }
+interface ColumnProps {
+  title: String;
+  cards: any[];
+}
 
-// const Column: React.FC = props => (
-//   <div className="c-column">
-//     <h3>{props.title}</h3>
-//     <ol className="c-board__columns">
-//       {props.cards.map(c => (
-//         <li>
-//           <Card {...c} />
-//         </li>
-//       ))}
-//     </ol>
-//   </div>
-// );
+const Column: React.FC<ColumnProps> = ({ title, cards = [] }) => (
+  <div className="c-column">
+    <h3>{title}</h3>
+    <ol className="c-board__columns">
+      {cards.map(c => (
+        <li>
+          <Card {...c} />
+        </li>
+      ))}
+    </ol>
+  </div>
+);
 
 interface BoardProps {
   data: BoardsQuery;
@@ -39,8 +41,7 @@ export const Board: React.FC<BoardProps> = ({ data }) => {
       <ol className="c-board__columns">
         {board.columns.map(c => (
           <li>
-            <pre>{JSON.stringify(c, null, 2)}</pre>
-            {/* <Column {...c} /> */}
+            <Column {...c} />
           </li>
         ))}
       </ol>
