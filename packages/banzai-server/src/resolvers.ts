c98486e0d5cbd80cc7ad9941f338b4c01459d793
Charles Oliver <charles@cocoa.consulting>
2019-07-06 13:41:00 +0100

    add schema and mock data resolvers

--- packages/banzai-server/src/resolvers.ts
+++ packages/banzai-server/src/resolvers.ts
@@ -1,7 +1,128 @@
+import { values } from "ramda";
+import { generate as id } from "shortid";
+
+const labels = {
+  dev: {
+    id: id(),
+    title: "Development",
+    color: "#0019FF",
+  },
+  design: {
+    id: id(),
+    title: "Design",
+    color: "#DB00FF",
+  },
+  planning: {
+    id: id(),
+    title: "Planning",
+    color: "#00E168",
+  },
+};
+
+const columns = [
+  {
+    title: "Todo",
+    id: id(),
+    cards: [
+      {
+        id: id(),
+        title: "create persistence layer",
+        labels: [labels.dev],
+      },
+      {
+        id: id(),
+        title: "find styled-components ui kit",
+        labels: [labels.dev, labels.design],
+      },
+      {
+        id: id(),
+        title: "configure lerna",
+        labels: [labels.dev],
+      },
+      {
+        id: id(),
+        title: "configure linting/formatting",
+        labels: [labels.dev],
+      },
+      {
+        id: id(),
+        title: "plan features",
+        labels: [labels.planning],
+      },
+    ],
+  },
+  {
+    title: "Doing",
+    id: id(),
+    cards: [
+      {
+        id: id(),
+        title: "create basic components",
+        labels: [labels.dev],
+      },
+      {
+        id: id(),
+        title: "create mock data",
+        labels: [labels.dev],
+      },
+    ],
+  },
+  {
+    title: "Done",
+    id: id(),
+    cards: [
+      {
+        id: id(),
+        title: "create schema",
+        labels: [labels.dev],
+      },
+      {
+        id: id(),
+        title: "create wireframes",
+        labels: [labels.design],
+      },
+      {
+        id: id(),
+        title: "create monorepo",
+        labels: [labels.dev],
+      },
+    ],
+  },
+];
+
+const user = {
+  id: id(),
+  name: "Charlie",
+  email: "cpoliver@gmail.com",
+  boards: [],
+  memberOf: [],
+};
+
+const org = {
+  id: id(),
+  name: "cloudThing",
+  owner: user,
+  boards: [],
+  members: [user],
+};
+
+const board = {
+  id: id(),
+  title: "Trello Replacement for Power-Users",
+  columns,
+  owner: user,
+  org,
+  permissions: [],
+};
+
 export const resolvers = {
   Query: {
     testMessage: (): string => {
       return "Hello, World!";
     },
+
+    boards: (): any => [board],
+    labels: (): any[] => values(labels),
+    users: (): any[] => [user],
   },
 };
--- packages/banzai-server/src/schema.ts
+++ packages/banzai-server/src/schema.ts
@@ -2,9 +2,77 @@ import { gql } from "apollo-server";
 
 export default gql`
   type Query {
-    """
-    Test Message.
-    """
     testMessage: String!
+
+    boards: [Board!]!
+    labels: [Label!]!
+    users: [User!]!
+  }
+
+  type Board {
+    id: ID!
+    title: String!
+    columns: [Column!]!
+    permissions: [Permission!]!
+    owner: User!
+    org: Org
+  }
+
+  type Column {
+    id: ID!
+    title: String!
+    cards: [Card!]!
+  }
+
+  type Card {
+    id: ID!
+    title: String!
+    description: String!
+    labels: [Label!]!
+    checklists: [Checklist!]!
+  }
+
+  type Label {
+    id: ID!
+    title: String!
+    description: String!
+    color: String!
+  }
+
+  type Checklist {
+    id: ID!
+    title: String
+    items: [ChecklistItem!]!
+  }
+
+  type ChecklistItem {
+    id: ID!
+    title: String!
+    isComplete: Boolean!
+  }
+
+  type Permission {
+    id: ID!
+    board: Board!
+    user: User
+    org: Org
+    view: Boolean!
+    edit: Boolean!
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    name: String!
+    boards: [Board!]!
+    memberOf: [Org!]!
+  }
+
+  type Org {
+    id: ID!
+    name: String!
+    owner: User!
+    members: [User!]!
+    boards: [Board!]!
   }
 `;
