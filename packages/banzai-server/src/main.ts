fde42ce39c557c6f4f964aff7ba985f49f49ece2
Charles Oliver <charles@cocoa.consulting>
2019-07-06 12:36:47 +0100

    add and configure dotenv

--- packages/banzai-server/src/main.ts
+++ packages/banzai-server/src/main.ts
@@ -1,11 +1,14 @@
 import { ApolloServer } from "apollo-server";
 
+import { environment } from "./environment";
 import { resolvers } from "./resolvers";
 import typeDefs from "./schema";
 
 const server = new ApolloServer({ resolvers, typeDefs });
 
-server.listen().then(({ url }) => console.log(`Server ready at ${url}. `));
+server
+  .listen(environment.port)
+  .then(({ url }) => console.log(`Server ready at ${url}. `));
 
 if (module.hot) {
   module.hot.accept();
