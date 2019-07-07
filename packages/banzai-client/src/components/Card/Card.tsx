c825996d1f4b1dd76bd11c4ac046188af78cec76
Charles Oliver <charles@cocoa.consulting>
2019-07-07 18:26:50 +0100

    add fontawesome. tweak styles

--- packages/banzai-client/src/components/Card/Card.tsx
+++ packages/banzai-client/src/components/Card/Card.tsx
@@ -1,14 +1,34 @@
 import React from "react";
-
 import * as R from "rebass";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 
 interface CardProps {
   id: String;
   title: String;
+  description?: String;
 }
 
-export const Card: React.FC<CardProps> = ({ title }) => (
-  <R.Card backgroundColor="#121212" borderRadius={4} p={2} m={2}>
-    <R.Text>{title}</R.Text>
+const lipsum = `
+  Lorem ipsum dolor sit amet, consectetuer adipiscing elit..
+  Donec odio. Quisque volutpat mattis eros. Nullam malesuada
+  erat ut turpis. Suspendisse... 
+`;
+
+export const Card: React.FC<CardProps> = ({ title, description = lipsum }) => (
+  <R.Card backgroundColor="#333" borderRadius={4} p={3} m={2}>
+    <R.Flex paddingTop={1} paddingBottom={2}>
+      <R.Text fontSize={1} flex={1}>
+        {title}
+      </R.Text>
+      <R.Box>
+        <FontAwesomeIcon size="xs" color="#8A8A8A" icon={faEllipsisH} />
+      </R.Box>
+    </R.Flex>
+    <R.Flex>
+      <R.Text fontSize={0} color="#8A8A8A">
+        {description}
+      </R.Text>
+    </R.Flex>
   </R.Card>
 );
--- packages/banzai-client/src/components/Column/Column.tsx
+++ packages/banzai-client/src/components/Column/Column.tsx
@@ -10,8 +10,8 @@ interface ColumnProps {
 }
 
 export const Column: React.FC<ColumnProps> = ({ title, cards }) => (
-  <R.Flex flexDirection="column" flex={1} backgroundColor="#191919" m={2} p={2}>
-    <R.Heading fontSize={3} padding={2} textAlign="center">
+  <R.Flex flexDirection="column" flex={1} backgroundColor="#1A1A1A" m={2} p={2}>
+    <R.Heading fontSize={2} padding={2} textAlign="center">
       {title} ({cards.length})
     </R.Heading>
     <R.Flex flexDirection="column">
