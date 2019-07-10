61b7cd6ee994a0a685a37c5027b65a90d2f85107
Charles Oliver <charles@cocoa.consulting>
2019-07-10 16:56:47 +0100

    add move selected state helper and tests

--- packages/banzai-client/src/state/state.test.ts
+++ packages/banzai-client/src/state/state.test.ts
@@ -1,54 +1,99 @@
-import { move, State } from './state';
+import { assocPath } from "ramda";
 
-const initState = {
-  selected: {
-    col: 0,
-    row: 0,
-    colCounts: [
-      3, 4, 2
-    ]
-  }
+import { move } from "./state";
+
+const board = {
+  columns: [
+    { title: "TODO", cards: { length: 8 } },
+    { title: "DOING", cards: { length: 4 } },
+    { title: "BLOCKED", cards: { length: 1 } },
+    { title: "DONE", cards: { length: 10 } },
+  ],
 };
 
-const calc = (state: State) => ({
-  canMoveLeft: () => 
-})
+const baseState = {
+  board,
+  selected: {
+    col: 0,
+    row: 7,
+  },
+};
 
-describe("", () => {
+describe("move", () => {
   test("should move left", () => {
-    const result = move(initState);
+    const state = assocPath(["selected"], { col: 1, row: 2 }, baseState);
+    const result = move(state, "left");
 
-    expect(result).toEqual(
-        { selected: {
-          col: 2,
-          row: 0,
-        }
-      });
+    expect(result).toEqual({
+      col: 0,
+      row: 2,
     });
   });
 
   test("should wrap around left", () => {
-    //
+    const result = move(baseState, "left");
+
+    expect(result).toEqual({
+      col: 3,
+      row: 7,
+    });
   });
+
   test("should move right", () => {
-    //
+    const result = move(baseState, "right");
+
+    expect(result).toEqual({
+      col: 1,
+      row: 3,
+    });
   });
 
   test("should wrap around right", () => {
-    //
+    const state = assocPath(["selected", "col"], 3, baseState);
+    const result = move(state, "right");
+
+    expect(result).toEqual({
+      col: 0,
+      row: 7,
+    });
   });
+
   test("should move up", () => {
-    //
+    const result = move(baseState, "up");
+
+    expect(result).toEqual({
+      col: 0,
+      row: 6,
+    });
   });
 
   test("should wrap around up", () => {
-    //
+    const state = assocPath(["selected", "row"], 0, baseState);
+    const result = move(state, "up");
+
+    expect(result).toEqual({
+      col: 0,
+      row: 7,
+    });
   });
+
   test("should move down", () => {
-    //
+    const state = assocPath(["selected", "row"], 4, baseState);
+    const result = move(state, "down");
+
+    expect(result).toEqual({
+      col: 0,
+      row: 5,
+    });
   });
 
   test("should wrap around down", () => {
-    //
+    const state = assocPath(["selected", "row"], 7, baseState);
+    const result = move(state, "down");
+
+    expect(result).toEqual({
+      col: 0,
+      row: 0,
+    });
   });
 });
--- packages/banzai-client/src/state/state.ts
+++ packages/banzai-client/src/state/state.ts
@@ -1,45 +1,92 @@
 import { cond, equals, evolve } from "ramda";
+import { mcall } from "q";
 
-interface State {
-  selected: {
-    col: number;
-    row: number;
-  };
-  last: {
-    row: number;
-    column: number;
-  };
+interface AppState {
+  board: BoardState;
+  selected: SelectedState;
 }
 
-export const initState = {
-  selected: {
-    col: 0,
-    row: 0,
-  },
-  last: {
-    col: 0,
-    row: 0,
-  },
+interface Column {
+  title: string;
+  cards: { length: number };
+}
+
+interface BoardState {
+  columns: Column[];
+}
+
+interface SelectedState {
+  col: number;
+  row: number;
+}
+
+interface MaxState {
+  col: number;
+  row: (col: number) => number;
+}
+
+export interface MoveInfo {
+  selected: SelectedState;
+  max: MaxState;
+}
+
+type Direction = "left" | "up" | "down" | "right";
+
+const moveLeft = ({ selected, max }: MoveInfo) => {
+  const newCol = selected.col - 1;
+  const col = newCol < 0 ? max.col : newCol;
+  const row = Math.min(max.row(col), selected.row);
+
+  return {
+    row,
+    col,
+  };
 };
 
-const DIRECTION = {
-  LEFT: "h",
-  DOWN: "j",
-  UP: "k",
-  RIGHT: "l",
+const moveDown = ({ selected, max }: MoveInfo) => {
+  const newRow = selected.row + 1;
+  const row = newRow > max.row(selected.col) ? 0 : newRow;
+
+  return {
+    ...selected,
+    row,
+  };
 };
 
-export const move = (state: State) =>
-  evolve({
-    selected: {
-      col: cond([
-        [equals(DIRECTION.LEFT), () => Math.max(0, state.selected.col - 1)],
-        [
-          equals(DIRECTION.DOWN),
-          () => Math.max(state.last.row, state.selected.row + 1),
-        ],
-        [equals(DIRECTION.UP), () => Math.max(0, state.selected.col - 1)],
-        [equals(DIRECTION.RIGHT), () => Math.max(0, state.selected.col - 1)],
-      ]),
-    },
-  })(state);
+const moveUp = ({ selected, max }: MoveInfo) => {
+  const newRow = selected.row - 1;
+  const row = newRow < 0 ? max.row(selected.col) : newRow;
+
+  return {
+    ...selected,
+    row,
+  };
+};
+
+const moveRight = ({ selected, max }: MoveInfo) => {
+  const newCol = selected.col + 1;
+  const col = newCol > max.col ? 0 : newCol;
+  const row = Math.min(max.row(col), selected.row);
+
+  return {
+    row,
+    col,
+  };
+};
+
+export const move = (
+  { selected, board }: AppState,
+  direction: Direction,
+): SelectedState => {
+  const max = {
+    col: board.columns.length - 1,
+    row: (col: number) => board.columns[col].cards.length - 1,
+  };
+
+  return cond([
+    [equals("left"), () => moveLeft({ selected, max })],
+    [equals("down"), () => moveDown({ selected, max })],
+    [equals("up"), () => moveUp({ selected, max })],
+    [equals("right"), () => moveRight({ selected, max })],
+  ])(direction);
+};
